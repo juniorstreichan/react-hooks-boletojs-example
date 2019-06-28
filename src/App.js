d4b6a0723a98bbafd394d6b18ba44f6d629ea4e5
Junior Erdmann Streichan <juniorstreichan@gmail.com>
2019-06-28 18:46:20 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,67 +1,79 @@
-import React, { useState } from "react";
-import "./App.css";
-import * as Boleto from "boleto.js";
+import React, { useState } from 'react'
+import './App.css'
+import * as Boleto from 'boleto.js'
 
-function App() {
-  const [code, setCode] = useState("");
+function App () {
+  const [code, setCode] = useState('')
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Boleto.js React Hooks example</h1>
       <input
-        type="text"
+        type='text'
         style={{
-          width: "80%",
-          height: "2em",
-          textAlign: "center",
-          fontSize: "1em"
+          width: '80%',
+          height: '2em',
+          textAlign: 'center',
+          fontSize: '1em'
         }}
-        placeholder="[ Digite o número do boleto aqui ]"
+        placeholder='[ Digite o número do boleto aqui ]'
         onChange={e => setCode(e.target.value)}
       />
       {code && <Ticket code={code} />}
+      <br />
+      <code style={{ color: '#999' }}>
+        test: 34195000080123320318964221470004584410000002000
+      </code>
     </div>
-  );
+  )
 }
 
-function Button(props) {
+function Button (props) {
   return (
-    <button style={{ padding: "5px", minWidth: "40px" }} {...props}>
+    <button style={{ padding: '5px', minWidth: '40px' }} {...props}>
       {props.children}
     </button>
-  );
+  )
 }
 
-function Ticket({ code }) {
+function Ticket ({ code }) {
   try {
-    const boleto = new Boleto(code);
-    const [percentBarcode, setBarCodeZoom] = useState(100);
+    const boleto = new Boleto(code)
+    const [percentBarcode, setBarCodeZoom] = useState(100)
 
     return (
-      <div style={{ padding: "2em", margin: "1em", border: "1px solid black" }}>
+      <div style={{ padding: '2em', margin: '1em', border: '1px solid black' }}>
         <table>
           <tbody>
             <tr>
-              <td style={{ textAlign: "right" }}>Banco:</td>
-              <td style={{ textAlign: "left" }}>
-                <b>{boleto.bank()}</b>
+              <td style={{ textAlign: 'right' }}>Banco:</td>
+              <td style={{ textAlign: 'left' }}>
+                <b>
+                  {boleto.bank()}
+                </b>
               </td>
             </tr>
             <tr>
-              <td style={{ textAlign: "right" }}>Valor:</td>
-              <td style={{ textAlign: "left" }}>
-                <b>{boleto.prettyAmount()}</b>
+              <td style={{ textAlign: 'right' }}>Valor:</td>
+              <td style={{ textAlign: 'left' }}>
+                <b>
+                  {boleto.prettyAmount()}
+                </b>
               </td>
             </tr>
             <tr>
-              <td style={{ textAlign: "right" }}>Data de validade:</td>
-              <td style={{ textAlign: "left" }}>
-                <b>{boleto.expirationDate().toLocaleDateString()}</b>
+              <td style={{ textAlign: 'right' }}>Data de validade:</td>
+              <td style={{ textAlign: 'left' }}>
+                <b>
+                  {boleto.expirationDate().toLocaleDateString()}
+                </b>
               </td>
             </tr>
           </tbody>
         </table>
         <hr />
-        <p>Zoom {percentBarcode}%</p>
+        <p>
+          Zoom {percentBarcode}%
+        </p>
         <Button
           disabled={percentBarcode <= 20}
           onClick={() => setBarCodeZoom(percentBarcode - 5)}
@@ -85,12 +97,14 @@ function Ticket({ code }) {
           style={{ width: `${percentBarcode}%` }}
           dangerouslySetInnerHTML={{ __html: boleto.toSVG() }}
         />
-        <p>{boleto.barcode()}</p>
+        <p>
+          {boleto.barcode()}
+        </p>
       </div>
-    );
+    )
   } catch (error) {
-    return <p style={{ color: "red" }}>Invalid bank slip number</p>;
+    return <p style={{ color: 'red' }}>Invalid bank slip number</p>
   }
 }
 
-export default App;
+export default App
