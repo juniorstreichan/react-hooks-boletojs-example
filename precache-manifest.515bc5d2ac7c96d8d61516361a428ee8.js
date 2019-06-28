2ce53f949274308b66f5a5f0780f763869192069
Junior Erdmann Streichan <juniorstreichan@gmail.com>
2019-06-28 18:44:41 -0400

    Updates

---
+++ precache-manifest.515bc5d2ac7c96d8d61516361a428ee8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "579b2f6b91032c5aba0c",
+    "url": "/boletojs/static/js/runtime~main.579b2f6b.js"
+  },
+  {
+    "revision": "f33167ea4d374b410895",
+    "url": "/boletojs/static/js/main.f33167ea.chunk.js"
+  },
+  {
+    "revision": "2c2f1608ada280015d2a",
+    "url": "/boletojs/static/js/2.2c2f1608.chunk.js"
+  },
+  {
+    "revision": "f33167ea4d374b410895",
+    "url": "/boletojs/static/css/main.5bd76090.chunk.css"
+  },
+  {
+    "revision": "2a76ba5d8913b1032ec8bdc5582cdaa0",
+    "url": "/boletojs/index.html"
+  }
+];
